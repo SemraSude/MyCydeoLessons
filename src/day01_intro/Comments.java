4fbead4b83e19d6d152e46820abae7dc0d45ec7a
SemraSude <semrasudeavci@gmail.com>
2023-08-13 12:20:19 +0300

    öğrenmelerim

--- src/day01_intro/Comments.java
+++ src/day01_intro/Comments.java
@@ -4,7 +4,7 @@ public class Comments {
     public static void main(String[] args) {
         System.out.println("I love coding"); // as semra i really like coding
 
-/* comment line 1
+/* comment line 1// bu satırı şimdi göreceksin
 comment line 2
  */
 // satırları düzenlemek için ctrl+alt+L
